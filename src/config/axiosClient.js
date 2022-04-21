38ad259d6e0d5ff06f9363bf2b7de9995eeed0d5
Kevin diaz <40407751+Kevindm14@users.noreply.github.com>
2022-04-21 14:04:53 -0500

    Update axiosClient.js

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://livegift-app.herokuapp.com/'
 })
 
 export default axiosClient
